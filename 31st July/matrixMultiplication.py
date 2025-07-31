6b62de2b1902c597eecdafb66b70057c061729f7
ANUBHAV KUMAR SAH <139559243+Anubhav-Sah@users.noreply.github.com>
2025-07-31 09:23:51 +0000

    added 31st july task

---
+++ 31st July/matrixMultiplication.py
@@ -0,0 +1,21 @@
+def matrix_multiplication(matrix1, matrix2, row1, row2, col1, col2):
+    if col1 != row2:
+        return -1  # Incompatible matrices for multiplication
+    # Initialize the multiplication matrix with zero values
+    multiplication_matrix = [[0 for _ in range(col2)] for _ in range(row1)]
+    
+    for i in range(row1):
+        for j in range(col2):
+            for k in range(col1):
+                multiplication_matrix[i][j] += matrix1[i][k] * matrix2[k][j]  # Accumulate the sum for each element
+    return multiplication_matrix
+
+matrix1 = [[1, 2],
+           [3, 4]]
+
+matrix2 = [[1, 2],
+           [3, 4]]
+
+result = matrix_multiplication(matrix1, matrix2, len(matrix1), len(matrix2), len(matrix1[0]), len(matrix2[0]))
+
+print(result)
